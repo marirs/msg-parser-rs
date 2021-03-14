5d856cc03ff396fd31eb1a3af9fa49aa175aacb6
Sriram Govindan <marirs@gmail.com>
2021-03-14 17:30:28 +0530

    tweaking the reading of headers

--- examples/parse-email.rs
+++ examples/parse-email.rs
@@ -2,10 +2,10 @@ use msg_parser::Outlook;
 
 fn main() {
     // Create Outlook object
-    let outlook = Outlook::from_path("data/test_email.msg").unwrap();
+    let outlook = Outlook::from_path("data/test_email_4.msg").unwrap();
 
     // Flush as json string
     let _json_string = outlook.to_json();
 
-    // println!("{:#?}", outlook);
+    println!("{:#?}", outlook);
 }
